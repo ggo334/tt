e37a6ab228c93942a09f2848ad32a6226151b786
ggo334 <ggo334538@gmail.com>
2026-09-03 23:01:21 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hallodunia;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
